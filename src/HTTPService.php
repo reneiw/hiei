c2b5e038d08d5bbea34ad41e7d71dc35dbe2081a
CHEN ZHEYU <sturm@live.in>
2020-10-27 17:26:49 +0800

    update HTTPService.php

---
+++ src/HTTPService.php
@@ -0,0 +1,101 @@
+<?php
+
+
+namespace Reneiw\Hiei;
+
+
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Exception\RequestException;
+use Psr\Http\Message\ResponseInterface;
+
+class HTTPService
+{
+    protected ClientInterface $client;
+
+    public function __construct(ClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
+    public function request(string $method, string $uri, array $params = null, array $headers = [], bool $sync = true)
+    {
+        // Build the request parameters for Guzzle
+        $guzzleParams = [];
+        if ($params !== null) {
+            $guzzleParams[strtoupper($method) === 'GET' ? 'query' : 'json'] = $params;
+        }
+
+        // Add custom headers
+        if (count($headers) > 0) {
+            $guzzleParams['headers'] = array_merge($guzzleParams['headers'], $headers);
+        }
+
+        /**
+         * Run the request as async.
+         */
+
+        if ($sync === false) {
+            // Async request
+            $promise = $this->getClient()->requestAsync($method, $uri, $guzzleParams);
+            return $promise->then([$this, 'handleSuccess'], [$this, 'handleFailure']);
+        }
+
+        /**
+         * Run the request as sync.
+         */
+
+        try {
+            return $this->handleSuccess($this->getClient()->request($method, $uri, $guzzleParams));
+        } catch (GuzzleException $e) {
+            return $this->handleFailure($e);
+        }
+    }
+
+    public function handleSuccess(ResponseInterface $resp): array
+    {
+        // Return Guzzle response and JSON-decoded body
+        return [
+            'errors' => false,
+            'body' => json_decode(trim($resp->getBody()), true),
+            'headers' => $resp->getHeaders(),
+            'response' => $resp,
+            'status' => $resp->getStatusCode(),
+        ];
+    }
+
+    public function handleFailure(GuzzleException $e): array
+    {
+        $resp = null;
+        $body = null;
+        $status = null;
+
+        if ($e instanceof RequestException) {
+            $resp = $e->getResponse();
+            if ($resp) {
+                // Get the body stream
+                $rawBody = $resp->getBody();
+                $status = $resp->getStatusCode();
+
+                // Build the error object
+                if ($rawBody !== null) {
+                    // Convert data to response
+                    $body = json_decode(trim($resp->getBody()), true);
+                }
+            }
+        }
+
+        return [
+            'errors' => true,
+            'response' => $resp,
+            'status' => $status,
+            'body' => $body,
+            'exception' => $e,
+        ];
+    }
+
+    public function getClient(): ClientInterface
+    {
+        return $this->client;
+    }
+}
