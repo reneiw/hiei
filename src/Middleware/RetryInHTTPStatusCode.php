5562302ab3aa6d1e12277290bca9a21de4885bfb
CHEN ZHEYU <sturm@live.in>
2020-10-28 10:10:57 +0800

    rename

--- src/Middleware/RetryInHTTPStatusCode.php
+++ src/Middleware/RetryInHTTPStatusCode.php
@@ -9,23 +9,22 @@ use GuzzleHttp\Exception\RequestException;
 use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Response;
 
-class RetryRequest
+class RetryInHTTPStatusCode
 {
     /**
      * @param $maxTries
      * @param  array|int[]  $codes
-     * @param  int  $delay
      *
      * @return Closure
      */
-    public function __invoke($maxTries, array $codes = [500, 502, 503, 504], $delay = 0): callable
+    public function __invoke(int $maxTries, array $codes = [500, 502, 503, 504]): callable
     {
         return function (
             $retries,
             Request $request,
             Response $response = null,
             RequestException $exception = null
-        ) use ($maxTries, $codes, $delay) {
+        ) use ($maxTries, $codes) {
             // 超过最大重试次数，不再重试
             if ($retries >= $maxTries) {
                 return false;
@@ -33,18 +32,12 @@ class RetryRequest
 
             // 请求失败，继续重试
             if ($exception instanceof ConnectException) {
-                if ($delay) {
-                    sleep($delay);
-                }
                 return true;
             }
 
             if ($response) {
                 // 如果请求有响应，但是状态码大于等于500，继续重试(这里根据自己的业务而定)
                 if (in_array($response->getStatusCode(), $codes)) {
-                    if ($delay) {
-                        sleep($delay);
-                    }
                     return true;
                 }
             }
